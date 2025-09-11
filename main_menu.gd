ac563461e9fd1c7c373924ff88b9a42cae93ab09
Mario Gonçalves de Freitas Junior <160534597+Mariojunior2@users.noreply.github.com>
2025-09-11 14:57:58 -0300

    Sistema com Settings funcional

--- main_menu.gd
+++ main_menu.gd
@@ -1,22 +1,34 @@
 extends Control
 
+@onready var main_buttons: VBoxContainer = $MainButtons
+@onready var options: Panel = $Options
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	main_buttons. visible = true
+	options. visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://word_1.tscn")
 
+func _on_settings_back_pressed() -> void:
+	main_buttons. visible = true
+	options. visible = false
+
 
 func _on_settings_pressed() -> void:
 	print("Configurações")
+	main_buttons. visible = false
+	options. visible = true
+	
 
 
 func _on_exit_pressed() -> void:
